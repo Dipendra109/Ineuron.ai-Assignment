c7e3e2036b4e7420e00e5ce2d9b5f0f8183fd838
Dipendra109 <102921561+Dipendra109@users.noreply.github.com>
2022-04-04 09:02:47 +0200

    Add files via upload

---
+++ assignment1_dipendrachoudhary.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+
+# Question1
+print("Questions 1 Answers as shown below")
+print("'*' Is a mathematical operator meant for multiplication. \n' 'hello' ' is an expression. It is a string. \n' -87.8 ' is a negative float. \n' - ' is a maths opertor of subtraction. \n' / ' is a maths opertor of division. \n' + ' is a maths opertor of division. \n' 6 ' is an integer number.")
+
+
+# In[24]:
+
+
+#Question2 
+print("Question 2 answer is" )
+print("String is a type of a variable. \nVariable is an object")
+
+
+# In[25]:
+
+
+#Question3
+print("Three data types are \n1.Integer \n2.Float \n3.String")
+
+
+# In[26]:
+
+
+#Question4
+print("Expression is a combination of variabes, constants and mathematical operators. It is run by a programming language.")
+
+
+# In[27]:
+
+
+#Question5
+print("A statement is storylines of aprogram written in langauge. It is use of syntax of a programming through which a action is carried out.")
+
+
+# In[28]:
+
+
+#Question6 
+bacon =22 
+bacon = bacon+1
+print(bacon)
+
+
+# In[30]:
+
+
+#Question7
+'spam' + 'spamspam' 
+'spam'*3
+#Both code lins indicated above will give same output
+
+
+# In[31]:
+
+
+#Question8
+print("Integers can not be defined as variables.") 
+
+
+# In[40]:
+
+
+#Question9
+print("Int() for integer. \nflot() for float number. \nstr() for a string. ")
+
+
+# In[39]:
+
+
+#question10
+print("In expression 'I have eaten' + 99 + 'burritos' , there is an integer. Whole expression should be made up of strings. It is necessary to convert 99 into string too.  ")
+'I have eaten ' + str(99) +' burritos'
+
+
+# In[ ]:
+
+
+
+
